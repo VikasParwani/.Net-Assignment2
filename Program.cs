c52914454e582888e955503ba6bda801c56106ed
VikasParwani <114342734+VikasParwani@users.noreply.github.com>
2024-11-11 23:39:14 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+
+public class Student
+{
+    public string Name { get; set; }
+    public int RollNo { get; set; }
+    public int[] Marks = new int[5];
+    public int TotalMarks { get; set; }
+    public double Percentage { get; set; }
+
+    public static string UniversityName = "Central University";
+
+    // Default Constructor
+    public Student()
+    {
+        Name = "Unknown";
+        RollNo = 0;
+        Marks = new int[5];
+        TotalMarks = 0;
+        Percentage = 0.0;
+    }
+
+    // Parameterized Constructor
+    public Student(string name, int rollNo, int[] marks)
+    {
+        Name = name;
+        RollNo = rollNo;
+        Marks = marks;
+        CalculateResult();
+    }
+
+    // Static Constructor
+    static Student()
+    {
+        Console.WriteLine("University Name: " + UniversityName);
+    }
+
+    // Read data from the user
+    public void ReadData()
+    {
+        Console.Write("Enter Name: ");
+        Name = Console.ReadLine();
+
+        Console.Write("Enter Roll No: ");
+        RollNo = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter Marks in 5 Subjects:");
+        for (int i = 0; i < 5; i++)
+        {
+            Marks[i] = int.Parse(Console.ReadLine());
+        }
+
+        CalculateResult();
+    }
+
+    public void CalculateResult()
+    {
+        TotalMarks = Marks.Sum();
+        Percentage = (double)TotalMarks / 500 * 100;
+    }
+
+    // Display student information
+    public void Display()
+    {
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Roll No: " + RollNo);
+        Console.WriteLine("Marks: " + string.Join(", ", Marks));
+        Console.WriteLine("Total Marks: " + TotalMarks);
+        Console.WriteLine("Percentage: " + Percentage + "%");
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,79 @@
+﻿using system;
+
+public interface IAccount
+{
+    void OpenAccount();
+    void CloseAccount();
+}
+
+public interface ICustomer
+{
+    void DisplayCustomerDetail();
+}
+
+public class SavingsAccount : IAccount, ICustomer
+{
+    public string CustomerName { get; set; }
+    public int AccountNumber { get; set; }
+    public double Balance { get; set; }
+
+    public static string BankName { get; set; } = "My Bank";
+
+    public SavingsAccount()
+    {
+        Console.WriteLine("Enter your name, A/C number, Balance");
+
+    }
+
+    public SavingsAccount(string name, int accountNumber, double balance)
+    {
+        CustomerName = name;
+        AccountNumber = accountNumber;
+        Balance = balance;
+    }
+
+    static SavingsAccount()
+    {
+        Console.WriteLine("Bank Name: " + BankName);
+    }
+
+    public void OpenAccount()
+    {
+        Console.WriteLine("Savings Account Opened");
+    }
+
+    public void CloseAccount()
+    {
+        Console.WriteLine("Savings Account Closed");
+    }
+
+    public void DisplayCustomerDetail()
+    {
+        Console.WriteLine("Customer Name: " + CustomerName);
+        Console.WriteLine("Account Number: " + AccountNumber);
+    }
+
+    public void Withdraw(double amount)
+    {
+        if (Balance >= amount)
+        {
+            Balance -= amount;
+            Console.WriteLine("Amount withdrawn: " + amount);
+        }
+        else
+        {
+            Console.WriteLine("Insufficient balance");
+        }
+    }
+
+    public void Deposit(double amount)
+    {
+        Balance += amount;
+        Console.WriteLine("Amount deposited: " + amount);
+    }
+
+    public void CheckAndDisplayBalance()
+    {
+        Console.WriteLine("Current Balance: " + Balance);
+    }
+}
